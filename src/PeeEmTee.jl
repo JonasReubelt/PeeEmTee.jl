e9511749353ebb334508914f5d5023a506a2ae9d
Jonas Reubelt <jonas.reujon.reubelt@fau.de>
2021-07-05 14:10:54 +0200

    fixes typo

--- src/PeeEmTee.jl
+++ src/PeeEmTee.jl
@@ -3,7 +3,7 @@ module PeeEmTee
 using HDF5
 using DocStringExtensions
 import Base
-export high_voltages, WaveSet, calculate_charges, ChargeDistribution
+export high_voltages, WaveSet, calculate_charges, ChargeDist
 
 include("tools.jl")
 
