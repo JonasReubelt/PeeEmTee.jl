6e45793fec50e88e6f322b4365cea64d76eeea24
Jonas Reubelt <jonas.reujon.reubelt@fau.de>
2021-07-06 18:20:59 +0200

    implements pre fit

--- src/PeeEmTee.jl
+++ src/PeeEmTee.jl
@@ -3,8 +3,11 @@ module PeeEmTee
 using HDF5
 using DocStringExtensions
 using StatsBase
+using Distributions
+using Optim
 import Base
-export high_voltages, WaveSet, calculate_charges, ChargeDist
+export high_voltages, WaveSet, calculate_charges, ChargeDist, pmtresp,
+       pre_fit, gauss
 
 include("tools.jl")
 
--- src/tools.jl
+++ src/tools.jl
@@ -77,3 +77,80 @@ end
     """
 ChargeDist(charges, bins::Integer; lower=-1, upper=1) = ChargeDist(charges, range(lower, upper; length=bins))
 
+
+"""
+    $(SIGNATURES)
+    normalized gaussian function
+"""
+norm_gauss(x, p) = exp.(.-(x .- p[1]).^2/(2 .*p[2] .^ 2)) ./ sqrt.(2π) ./ p[2]
+
+
+"""
+    $(SIGNATURES)
+    gaussian function
+"""
+gauss(x, p) = p[3] .* exp.(.-(x .- p[1]).^2/(2 .*p[2] .^ 2)) ./ sqrt.(2π) ./ p[2]
+
+
+"""
+    $(SIGNATURES)
+    PMT response function
+"""
+function pmtresp(x, p)
+    poisson = Poisson(abs(p[5]))
+    n_gaussians = 10
+    pedestal = pdf(poisson, 0) .* gauss.(x, p[1], p[2])
+    signal = zeros(length(x))
+    for i in 1:n_gaussians
+        signal .+= pdf(poisson, i) .* gauss.(x, p[3] * i, sqrt(i) * p[4])
+    end
+    p[6] .* (pedestal .+ signal)
+end
+
+"""
+    $(SIGNATURES)
+    quality function constructor for least square optimization
+"""
+function make_qfunc(model, x, y)
+    function qfunc(p)
+        mask = (y .!= 0)
+        model_res = model(x, p)
+        sum(((model_res[mask] .- y[mask]).^2) ./ y[mask])
+    end
+    qfunc
+end
+
+
+struct PreFitResults
+    μₚ
+    σₚ
+    Aₚ
+    μₛ
+    σₛ
+    Aₛ
+end
+
+
+"""
+    $(SIGNATURES)
+    performs pre fit
+"""
+function pre_fit(chargedist::ChargeDist)
+    x = chargedist.x
+    y = chargedist.y
+    qfunc = make_qfunc(gauss, x, y)
+    mxval_ped, mxidx_ped = findmax(y)
+    p0 = [x[mxidx_ped], 0.01, mxval_ped]
+    fit_ped = optimize(qfunc, p0, NewtonTrustRegion())
+    popt_ped = Optim.minimizer(fit_ped)
+    
+    mask = (x .> popt_ped[1] + 5 * popt_ped[2])
+    x_spe = x[mask]
+    y_spe = y[mask]
+    qfunc = make_qfunc(gauss, x_spe, y_spe)
+    mxval_spe, mxidx_spe = findmax(y_spe)
+    p0 = [x_spe[mxidx_spe], 0.1, mxval_spe]
+    fit_spe = optimize(qfunc, p0, NewtonTrustRegion())
+    popt_spe = Optim.minimizer(fit_spe)
+    PreFitResults(popt_ped..., popt_spe...)
+end
