b9d76f425b2b2a94d10e41130e88d60f10a60a69
Jonas Reubelt <jonas.reujon.reubelt@fau.de>
2021-07-13 15:19:42 +0200

    includes fit.jl and plot.jl

--- src/PeeEmTee.jl
+++ src/PeeEmTee.jl
@@ -12,5 +12,7 @@ export high_voltages, WaveSet, calculate_charges, ChargeDist, pmtresp,
        pre_fit, gauss, pmtresp_fit, pmtresp_uap, plot, plot!, calculate_transit_times, bin_data
 
 include("tools.jl")
+include("fit.jl")
+include("plot.jl")
 
 end # module
