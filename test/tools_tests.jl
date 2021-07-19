e07793ed0a687573e1aef07c0ac66464d44d3d42
JonasReubelt <jonas.reubelt@physik.uni-erlangen.de>
2021-07-19 15:26:51 +0200

    adds high_voltages test

--- test/tools_tests.jl
+++ test/tools_tests.jl
@@ -3,6 +3,9 @@ using Test
 
 const pmt_data = joinpath(@__DIR__, "data", "pmt_data.h5")
 
+@testset "high_voltages()" begin
+    @test high_voltages(pmt_data) == ["1000", "1100", "1200"]
+end
 
 @testset "WaveSet" begin
     a = [[1, 2, 3] [4, 5, 6] [7, 8, 9]]
