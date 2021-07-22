142b180f8a8d33a9e36c85e5b0d0723fe3a943da
Jonas Reubelt <jonas.reujon.reubelt@fau.de>
2021-07-22 15:51:33 +0200

    adds DelimitedFiles dependency

--- src/PeeEmTee.jl
+++ src/PeeEmTee.jl
@@ -6,11 +6,12 @@ using StatsBase
 using Distributions
 using Optim
 using RecipesBase
+using DelimitedFiles
 import Base
 export high_voltages, WaveSet, calculate_charges, ChargeDist, pmtresp,
        pre_fit, gauss, norm_gauss, pmtresp_fit, pmtresp_uap, plot, plot!,
        calculate_transit_times, bin_data, subtract_baseline, simulate_charges,
-       baseline_max, PMTRespFit, PMTRespUapFit
+       baseline_max, PMTRespFit, PMTRespUapFit, make_qfunc
 
 include("tools.jl")
 include("fit.jl")
