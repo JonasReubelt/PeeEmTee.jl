69aa079a49a97382dee9452213c566a29b9bced3
Jonas Reubelt <jonas.reujon.reubelt@fau.de>
2021-07-22 14:28:19 +0200

    adds another test for pmtresp

--- src/PeeEmTee.jl
+++ src/PeeEmTee.jl
@@ -10,7 +10,7 @@ import Base
 export high_voltages, WaveSet, calculate_charges, ChargeDist, pmtresp,
        pre_fit, gauss, norm_gauss, pmtresp_fit, pmtresp_uap, plot, plot!,
        calculate_transit_times, bin_data, subtract_baseline, simulate_charges,
-       baseline_max
+       baseline_max, PMTRespFit
 
 include("tools.jl")
 include("fit.jl")
