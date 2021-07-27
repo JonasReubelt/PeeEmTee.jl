46a1050544f8d6745e40cec6de4e8202fa839c87
Jonas Reubelt <jonas.reujon.reubelt@fau.de>
2021-07-27 15:27:09 +0200

    fixes fit tests

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 [![Codecov](https://codecov.io/gh/JonasReubelt/PeeEmTee.jl/branch/main/graph/badge.svg)](https://codecov.io/gh/JonasReubelt/PeeEmTee.jl)
 
 
-The PeeEmTee Julia package makes the analysis of PMT waveform data fast and easy. Included are convenient reading of HDF5 data, several auxiliary structures and functions to simplify the data processing and routines to fit e.g. the PMT response function to a PMT charge distribution.
+The PeeEmTee Julia package makes the analysis of PMT waveform data easy and fast. Included are convenient reading of HDF5 data, several auxiliary structures and functions to simplify the data processing and routines to fit e.g. the PMT response function to a PMT charge distribution.
--- test/fit_tests.jl
+++ test/fit_tests.jl
@@ -19,34 +19,40 @@ end
 @testset "pre_fit()" begin
     data = readdlm(chargedist_file)
     chargedist = ChargeDist(data[:, 1], data[:, 2])
-    prefit = PreFit(-0.0009439575776608711,
-                    0.012781642902921997,
-                    455.31645300021285,
-                    0.20214663928517476,
-                    0.10553835465661239,
-                    46.63963250021425)
-    @test pre_fit(chargedist) == prefit
+    prefit_target = PreFit(-0.0009439575776608711,
+                           0.012781642902921997,
+                           455.31645300021285,
+                           0.20214663928517476,
+                           0.10553835465661239,
+                           46.63963250021425)
+    prefit_actual = pre_fit(chargedist)
+    @test isapprox([getfield(prefit_target, field) for field in fieldnames(PreFit)],
+                   [getfield(prefit_actual, field) for field in fieldnames(PreFit)])
 end
 
 @testset "pmtresp_fit()" begin
     data = readdlm(chargedist_file)
     chargedist = ChargeDist(data[:, 1], data[:, 2])
-    prffit = PMTRespFit(-0.0009971235435351222,
-                        0.0126624107013663,
-                        0.1999583888727531,
-                        0.09968193673964637,
-                        0.0976846106250966,
-                        499.58202790610073)
+    prffit_target = PMTRespFit(-0.0009971235435351222,
+                               0.0126624107013663,
+                               0.1999583888727531,
+                               0.09968193673964637,
+                               0.0976846106250966,
+                               499.58202790610073)
     prefit = pre_fit(chargedist)
-    @test pmtresp_fit(chargedist, prefit) == prffit
-    prfuapfit = PMTRespUapFit(-0.0010866013489182497,
-                              0.012573335066167545,
-                              0.20255116463024672,
-                              0.09761207500313138,
-                              0.09719044076647844,
-                              495.05266162357447,
-                              0.012985248350779485,
-                              0.021308630425982703,
-                              4.592234142351302)
-    @test pmtresp_fit(chargedist, prefit, mod=:uap) == prfuapfit
+    prffit_actual = pmtresp_fit(chargedist, prefit)
+    @test isapprox([getfield(prffit_target, field) for field in fieldnames(PMTRespFit)],
+                   [getfield(prffit_actual, field) for field in fieldnames(PMTRespFit)])
+    prfuapfit_target = PMTRespUapFit(-0.0010866013489182497,
+                                     0.012573335066167545,
+                                     0.20255116463024672,
+                                     0.09761207500313138,
+                                     0.09719044076647844,
+                                     495.05266162357447,
+                                     0.012985248350779485,
+                                     0.021308630425982703,
+                                     4.592234142351302)
+    prfuapfit_actual = pmtresp_fit(chargedist, prefit; mod=:uap)
+    @test isapprox([getfield(prfuapfit_target, field) for field in fieldnames(PMTRespUapFit)],
+                   [getfield(prfuapfit_actual, field) for field in fieldnames(PMTRespUapFit)])
 end
